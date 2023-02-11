2ff37654e2619d81d917f2ad8e35c1244d170843
rudimuliawan <muliawan.rudi91@gmail.com>
2023-02-12 02:21:39 +0700

    complete chapter 2.3

---
+++ Exercise-17.rkt
@@ -0,0 +1,8 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname Exercise-17) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(require 2htdp/image)
+
+(define (image-classify img)
+  (if (= (image-width img) (image-height img)) "square"
+      (if (> (image-width img) (image-height img)) "wide" "tall")))
